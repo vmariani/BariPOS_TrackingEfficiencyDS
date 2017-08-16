fa9c47dff157a211d661514845fa0c90c3c258ec
vmariani <vmariani@cern.ch>
2017-08-16 11:52:53 +0200

    last

--- Tracking/plugins/MC13_2btree.cc
+++ Tracking/plugins/MC13_2btree.cc
@@ -178,24 +178,16 @@ class MC13_2btree : public edm::EDAnalyzer {
  edm::EDGetToken genParticles_;
  edm::EDGetToken hPU_;
  
- TTree *Tree;
+ //define a new tree for the reco quantities
+ 
  TTree *genTree;
  double Pis_pt_gen, Pis_eta_gen, Pis_phi_gen;
  double Pi_pt_gen, Pi_eta_gen, Pi_phi_gen;
  double K_pt_gen, K_eta_gen, K_phi_gen;
  double DS_pt_gen, DS_eta_gen, DS_phi_gen;
  double D0_pt_gen, D0_eta_gen, D0_phi_gen;
- double Pis_pt, Pis_eta, Pis_phi;
- double Pi_pt, Pi_eta, Pi_phi;
- double K_pt, K_eta, K_phi;
- double DS_pt, DS_eta, DS_phi;
- double D0_pt, D0_eta, D0_phi;
- double CL_vertex;
- double x_p, y_p, z_p;
- double x_s, y_s, z_s;
- double L_abs, L_sigma;
- double cos_phi;
- int vertex_size, num_tracks, truePU, pt_tracks;
+
+ // define here the variables you want to put inside the tree as done above for the gen variables
 
  // ----------member data ---------------------------
 };
@@ -232,45 +224,8 @@ MC13_2btree::MC13_2btree(const edm::ParameterSet& iConfig)
  genTree->Branch("DS_eta_gen",&DS_eta_gen,"DS_eta_gen/D");
  genTree->Branch("DS_phi_gen",&DS_phi_gen,"DS_phi_gen/D");
 
- Tree= fs->make<TTree>("Analysis","Analysis");
-
- Tree->Branch("Pis_pt",&Pis_pt,"Pis_pt/D");
- Tree->Branch("Pis_eta",&Pis_eta,"Pis_eta/D");
- Tree->Branch("Pis_phi",&Pis_phi,"Pis_phi/D");
+ // do the same for the reco tree, calling each branch you want to define
  
- Tree->Branch("Pi_pt",&Pi_pt,"Pi_pt/D");
- Tree->Branch("Pi_eta",&Pi_eta,"Pi_eta/D");
- Tree->Branch("Pi_phi",&Pi_phi,"Pi_phi/D");
- 
- Tree->Branch("K_pt",&K_pt,"K_pt/D");
- Tree->Branch("K_eta",&K_eta,"K_eta/D");
- Tree->Branch("K_phi",&K_phi,"K_phi/D");
- 
- Tree->Branch("DS_pt",&DS_pt,"DS_pt/D");
- Tree->Branch("DS_eta",&DS_eta,"DS_eta/D");
- Tree->Branch("DS_phi",&DS_phi,"DS_phi/D");
- 
- Tree->Branch("D0_pt",&D0_pt,"D0_pt/D");
- Tree->Branch("D0_eta",&D0_eta,"D0_eta/D");
- Tree->Branch("D0_phi",&D0_phi,"D0_phi/D");
- 
- Tree->Branch("CL_vertex",&CL_vertex,"CL_vertex/D");
- 
- Tree->Branch("x_p",&x_p,"x_p/D");
- Tree->Branch("y_p",&y_p,"y_p/D");
- Tree->Branch("z_p",&z_p,"z_p/D");
- 
- Tree->Branch("x_s",&x_s,"x_s/D");
- Tree->Branch("y_s",&y_s,"y_s/D");
- Tree->Branch("z_s",&z_s,"z_s/D");
- 
- Tree->Branch("L_abs",&L_abs,"L_abs/D");
- Tree->Branch("L_sigma",&L_sigma,"L_sigma/D");
- Tree->Branch("cos_phi",&cos_phi,"cos_phi/D");
- Tree->Branch("vertex_size",&vertex_size,"vertex_size/I");
- Tree->Branch("pt_tracks",&pt_tracks,"pt_tracks/D");
- Tree->Branch("truePU",&truePU,"truePU/I");
- Tree->Branch("num_tracks",&num_tracks,"num_tracks/I");
 }
 
 MC13_2btree::~MC13_2btree()
@@ -412,197 +367,19 @@ MC13_2btree::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 
     if(goodvertex){
 
-     reco::TransientTrack  Ktrans;
-     reco::TransientTrack  pitrans;
-     std::vector<reco::TransientTrack> tks;
-     TLorentzVector D0candidates;
-     vector<reco::Track> myvector;
-     reco::Track tr1;
-     reco::Track tr2; 
-     reco::Track goodTrack1;
-     reco::Track goodTrack2;
-     TLorentzVector Kvector;
-     TLorentzVector Pivector;
-     TLorentzVector D0vector;
-     TLorentzVector Pisvector;
-     TLorentzVector DSvector;
-     TransientVertex v;
 
 				  //######### tracks preselection ##########
 
-     for (reco::TrackCollection::const_iterator track = tracks->begin();  track != tracks->end();  ++track){
-      ntracks ++;
-      pttracks = pttracks + track->pt();
-      if((track->pt() > 0.5) && ( (track->chi2())/(track->ndof()) <= 2.5) && (track->numberOfValidHits() >= 5) && (track->hitPattern().numberOfValidPixelHits() >= 2) && fabs(track->dxy(primVertex.position())) < 0.1 && fabs(track->dz(primVertex.position())) < 1 && (track->quality(Track::highPurity))) { 
-       myvector.push_back(*track);	
-      }
-     }
       			//############ combinatorial #############
-     if (myvector.size() > 1){ 
-      for (reco::TrackCollection::const_iterator track = myvector.begin();  track != myvector.end();  ++track) {
-       for (reco::TrackCollection::const_iterator track1 = track + 1; track1 != myvector.end(); ++track1){
-        if (track->charge() + track1->charge() == 0){	
-         goodtracks=false;
-         inv_mass1 = 0;
-         tr1 = *track;
-         tr2 = *track1;
-         if ((track->charge()== 1) && (track1->charge()==-1)){
-          Kvector.SetPtEtaPhiM(track1->pt(), track1->eta(), track1->phi(), K_mass);
-          Pivector.SetPtEtaPhiM(track->pt(), track->eta(), track->phi(), pi_mass);
-          D0vector = Kvector + Pivector;	
-          inv_mass1 = D0vector.M();
-          goodtracks = true;
-          goodTrack1 = *track;
-          goodTrack2 = *track1;
-          Ktrans = (*theB).build(tr1);
-          pitrans = (*theB).build(tr2);
-          }
-         else if ((track->charge()==-1) && (track1->charge()== 1) ){
-          Kvector.SetPtEtaPhiM(track->pt(), track->eta(), track->phi(), K_mass);
-          Pivector.SetPtEtaPhiM(track1->pt(), track1->eta(), track1->phi(), pi_mass);
-          D0vector = Kvector + Pivector;
-          inv_mass1 = D0vector.M();
-          goodtracks = true;
-          goodTrack1 = *track;
-          goodTrack2 = *track1;
-          Ktrans  = (*theB).build(tr1);
-          pitrans = (*theB).build(tr2);
-         }
-         if (fabs(inv_mass1 - D0_mass) < 0.1){
-          v_chi2 = 0;
-          CL = 0;
-          sameVertex = 0;
-          tks.clear();
-          tks.push_back(Ktrans); 
-          tks.push_back(pitrans);
-          if (tks.size() > 1){
-           KalmanVertexFitter kalman(true);
-           if(goodtracks){ v = kalman.vertex(tks); 
-            if(v.isValid()){sameVertex++;
-             v_chi2 = v.normalisedChiSquared();
-             CL = TMath::Prob(v.totalChiSquared(),(int)v.degreesOfFreedom());
-            } 
-	       }
-          }
-       
-          if(sameVertex > 0 && CL > 0.01 && goodtracks == true){
-           xprim = primVertex.position().x();
-           yprim = primVertex.position().y();
-           zprim = primVertex.position().z();
-           xsec = v.position().x();
-           ysec = v.position().y();
-           zsec = v.position().z();
-           dx = xsec - xprim;
-           dy = ysec - yprim;
-           dz = zsec - zprim;
-           L_abs = sqrt(pow(dx,2) + pow(dy,2) + pow(dz,2));
-           px = D0vector.Px();
-           py = D0vector.Py();
-           pz = D0vector.Pz();
-           pi = D0vector.P();
-           cosphi = (px*dx + py*dy + pz*dz)/(L_abs*pi);
-           L = (px*dx + py*dy + pz*dz)/pi;
 
-           if (cosphi > 0.9){
-           //covariance matrix SV
-            cov_sv[0][0] = v.positionError().cxx();
-            cov_sv[1][0] = v.positionError().cyx();
-            cov_sv[2][0] = v.positionError().czx();
-            cov_sv[0][1] = cov_sv[1][0];
-            cov_sv[1][1] = v.positionError().cyy();
-            cov_sv[2][1] = v.positionError().czy();
-            cov_sv[0][2] = cov_sv[2][0];
-            cov_sv[1][2] = cov_sv[2][1];
-            cov_sv[2][2] = v.positionError().czz();
-        
-           //covariance matrix PV
-            cov_pv[0][0] = primVertex.covariance(0,0);
-            cov_pv[1][0] = primVertex.covariance(1,0);
-            cov_pv[2][0] = primVertex.covariance(2,0);
-            cov_pv[0][1] = cov_pv[1][0];
-            cov_pv[1][1] = primVertex.covariance(1,1);
-            cov_pv[2][1] = primVertex.covariance(2,1);
-            cov_pv[0][2] = cov_pv[2][0];
-            cov_pv[1][2] = cov_pv[2][1];
-            cov_pv[2][2] = primVertex.covariance(2,2);    
-            deriv[0] = dx/L_abs;
-            deriv[1] = dy/L_abs;
-            deriv[2] = dz/L_abs;
-
-            if(D0vector.Pt() > 3){
+                // D0 candidates selection
 
               // third track selection 
 
-             for(reco::TrackCollection::const_iterator track3 = tracks->begin();  track3 != tracks->end();  ++track3){ 
-              if(track3->charge() == 1 && (track3->pt() > 0.3) && (track3->numberOfValidHits() >= 2) && fabs(track3->dxy(primVertex.position())/track3->dxyError()) < 3 && fabs(track3->dz(primVertex.position())/track3->dzError()) < 3 && ((track3->chi2())/(track3->ndof()) <= 3) ) {   
-               if(track3->pt() != Pivector.Pt() && track3->eta() != Pivector.Eta()){          
-	        Pisvector.SetPtEtaPhiM(track3->pt(), track3->eta(), track3->phi(), pi_mass);
-                Pis_pt = Pisvector.Pt();
-                DSvector = Pisvector + Kvector + Pivector;
-
-                if (fabs(DSvector.M() - D0vector.M()) < 0.160){
-                 if(DSvector.Pt() > 4.){
-               
-                  Pis_pt=Pisvector.Pt();
-                  Pis_eta=Pisvector.Eta();
-                  Pis_phi=Pisvector.Phi();
-                  Pi_pt=Pivector.Pt();
-                  Pi_eta=Pivector.Eta();
-                  Pi_phi=Pivector.Phi();
-
-                  K_pt=Kvector.Pt();
-                  K_eta=Kvector.Eta();
-                  K_phi=Kvector.Phi();
-                  DS_pt=DSvector.Pt();
-                  DS_eta=DSvector.Eta();
-                  DS_phi=DSvector.Phi();
-                  D0_pt=D0vector.Pt();
-                  D0_eta=D0vector.Eta();
-                  D0_phi=D0vector.Phi();
-
-                  CL_vertex = CL;
-                  vertex_size = hVtx->size();
-                  cos_phi = (px*dx + py*dy + pz*dz)/(L_abs*pi);
-                  x_p = xprim;
-                  y_p = yprim;
-                  z_p = zprim;
-
-                  x_s = xsec;
-                  y_s = ysec;
-                  z_s = zsec;
-                  L = (px*dx + py*dy + pz*dz)/pi;
-                  sigma_L = 0;
-                  for (int m = 0; m < 3; ++m){
-                   for (int n = 0; n < 3; ++n ){
-                    sigma_L += deriv[m]*deriv[n]*(cov_pv[m][n] + cov_sv[m][n]);
-                   }
-                  }
-                  sigma_L = sqrt(sigma_L);
-                  L_sigma = L/sigma_L;
-                  num_tracks = ntracks;
-                  pt_tracks = pttracks;
-
-                  //filling tree
-
-                  Tree->Fill();                 
-                 
-                  } //pt dstar
-                 }//diff < 160			 
-                }//pislow != pi
-               } // third track
-              }//for 
-             }//D0 pt
-            }//coseno
-           }// same vertex 
-          }//D0 mass
-         }//if total charge 0
-        }//for track 1 
-       }// for track
-      }//myvector >1
-     }//good vertex
-   }//D0
-  } //D* 
- }//#vertices > 0
+    }//good vertex
+   }//#vertices > 0
+  } //D0 
+ }//D*
 }
 
 // ------------ method called once each job just before starting event loop  ------------
